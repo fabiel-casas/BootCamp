b3065fa060da791f458d1a6805c4ac91d8f30790
Fabiel <fabiel016@gmail.com>
2018-02-08 21:52:56 -0500

    first example

--- app/src/androidTest/java/com/casas/fabiel/testapp/GeometryTest.java
+++ app/src/androidTest/java/com/casas/fabiel/testapp/GeometryTest.java
@@ -5,11 +5,37 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.casas.fabiel.testapp.GeometryShapeUtil.DIAMOND;
+import static com.casas.fabiel.testapp.GeometryShapeUtil.SQUARE;
+import static com.casas.fabiel.testapp.GeometryShapeUtil.TRIANGLE;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(AndroidJUnit4.class)
 public class GeometryTest {
 
     @Test
     public void createASquare() {
+        String shape = GeometryShapeUtil.createShape(SQUARE, 5);
+        assertEquals(shape, new StringBuilder().append("***\n").
+                append("***\n").
+                append("***\n").toString());
+    }
 
+    @Test
+    public void createATriangle() {
+        String shape = GeometryShapeUtil.createShape(TRIANGLE, 5);
+        assertEquals(shape, new StringBuilder().append("  *  \n").
+                append(" ***   \n").
+                append("*****\n").toString());
+    }
+
+    @Test
+    public void createADiamond() {
+        String shape = GeometryShapeUtil.createShape(DIAMOND, 5);
+        assertEquals(shape, new StringBuilder().append(" * \n").
+                append(" ***  \n").
+                append("*****\n").
+                append(" *** \n").
+                append("  *  \n").toString());
     }
 }
--- app/src/main/java/com/casas/fabiel/testapp/GeometryShapeUtil.java
+++ app/src/main/java/com/casas/fabiel/testapp/GeometryShapeUtil.java
@@ -1,9 +1,41 @@
 package com.casas.fabiel.testapp;
 
 public class GeometryShapeUtil {
+    public final static int SQUARE = 0;
+    public final static int TRIANGLE = 1;
+    public final static int DIAMOND = 2;
 
     public static String createShape(int shape, int size) {
-        return new StringBuilder().append("*****").
-                                    append("*****")
+        switch (shape) {
+            case SQUARE:
+                return createSquare(size);
+            case TRIANGLE:
+                return createTriangle(size);
+            case DIAMOND:
+                return createDiamond(size);
+            default:
+                return createSquare(size);
+        }
+
+    }
+
+    private static String createDiamond(int size) {
+        return new StringBuilder().append(" * \n").
+                                append(" ***  \n").
+                                append("*****\n").
+                                append(" *** \n").
+                                append("  *  \n").toString();
+    }
+
+    private static String createTriangle(int size) {
+        return new StringBuilder().append("  *  \n").
+                                   append(" ***   \n").
+                                   append("*****\n").toString();
+    }
+
+    private static String createSquare(int size) {
+        return new StringBuilder().append("***\n").
+                append("***\n").
+                append("***\n").toString();
     }
 }
